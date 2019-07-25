2bf0a285e2f4f082631936203138372b2b33c71f
tiantangtl@foxmail.com <tiantangtl@foxmail.com>
2019-07-25 16:40:37 +0800

    variable3

--- Python实验室/可变变量与不变变量/variable.py
+++ Python实验室/可变变量与不变变量/variable.py
@@ -33,19 +33,45 @@
 # b = [1, 2, 3]
 # b[0] = 2 # 正确，list可以修改颜色
 
-a = (1, 2)
-b = (1, 2)
+# a = (1, 2)
+# b = (1, 2)
 
-c = [1, 2]
-d = [1, 2]
+# c = [1, 2]
+# d = [1, 2]
 
-print(id(a))
-print(id(b))
-print(a == b)
-print(id(c))
-print(id(d))
-print(c is d)
+# print(id(a))
+# print(id(b))
+# print(a == b)
+# print(id(c))
+# print(id(d))
+# print(c is d)
 
 # However, for the sake of optimization (mostly) there are some exceptions 
 # for small integers (between -5 and 256) and small strings (interned strings, 
 # with a special length (usually less than 20 character)) 
+
+# a = (1, 2)
+# b = [1, 2]
+
+# print("The address of a: %d" % id(a))
+# a += (3,)
+# print("The address of a: %d" % id(a))
+
+# print("The address of b: %d" % id(b))
+# b += (3,)
+# print("The address of b: %d" % id(b))
+
+# a = 3
+# print("The address of a: %d" % id(a))
+# a += 3
+# print("The address of a: %d" % id(a))
+
+# a = (1, [1, 2])
+# a[1] = [3,4]
+# a[1].append(5)
+# print(a)
+
+
+a = -6
+b = -6
+print( a is b)
